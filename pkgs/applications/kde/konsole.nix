a0f5b9062fa64d01b615542eb0e9d1a602315598
Ivan Kozik <ivan@ludios.org>
2019-12-05 20:35:44 +0000

    konsole: revert 19.08.2's bad hidpi scaling changes

--- pkgs/applications/kde/konsole.nix
+++ pkgs/applications/kde/konsole.nix
@@ -1,5 +1,5 @@
 {
-  mkDerivation, lib,
+  mkDerivation, lib, fetchpatch,
   extra-cmake-modules, kdoctools,
   kbookmarks, kcompletion, kconfig, kconfigwidgets, kcoreaddons, kguiaddons,
   ki18n, kiconthemes, kinit, kdelibs4support, kio, knotifications,
@@ -24,6 +24,25 @@ mkDerivation {
 
   patches = [
     ./konsole-focus-right-tab-on-close.patch
+
+    # konsole 19.08.1 -> 19.08.2 introduced some bad hidpi scaling changes
+    # that introduced space between lines and also made it impossible to use
+    # a decimal size e.g. 9.5pt font.
+    (fetchpatch {
+      url = "https://github.com/KDE/konsole/commit/54820e0ff2745add8b8353f538ad67d66b657d49.patch";
+      sha256 = "1qi1cxvlm5nh58mpdsbcn8vkkfgz7f42an51kvd7bbzhmj8z8y2c";
+      revert = true;
+    })
+    (fetchpatch {
+      url = "https://github.com/KDE/konsole/commit/36cd1aa267cf616d8135e38f4da7e4142f64059a.patch";
+      sha256 = "1z62qhxcn1857i6nkwmj07qxir331b440pvfbfcnl4l08gnh4k3y";
+      revert = true;
+    })
+    (fetchpatch {
+      url = "https://github.com/KDE/konsole/commit/a3bc2f9667696a37b291852a2df851433cc4377b.patch";
+      sha256 = "0qq1xik3p0gjf5xxsqyrv53dy7lv5wnff2q4wicairhar1h3dcmg";
+      revert = true;
+    })
   ];
   postPatch = ''
     # Use a more visible color to indicate tab activity
